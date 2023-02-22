ef0684b5b7c8d91cee18b21e607db072dee09f11
Abhinav Gupta <mail@abhinavg.net>
2023-02-22 15:13:04 -0800

    Automatic titles for pages without one
    For pages that don't have a title (single h1 heading),
    generate one.
    
    If such a title is being generated
    and all other headings in the page are h2 or higher,
    leave them as-is
    
    However, if the page doesn't have a title
    because it has multiple h1 headings,
    push them all down one level to make room for the new title.
    
    Resolves #9

--- generate.go
+++ generate.go
@@ -70,5 +70,11 @@ func (g *generator) renderGroupItem(group *markdownGroupItem) error {
 }
 
 func (g *generator) renderFileItem(file *markdownFileItem) error {
+	if t := file.TOCTitle; t != nil {
+		if err := g.Renderer.Render(g.W, file.File.Source, t.AST.Node); err != nil {
+			return err
+		}
+		io.WriteString(g.W, "\n\n")
+	}
 	return g.Renderer.Render(g.W, file.File.Source, file.File.AST.Node)
 }
