4d859db02c917cf6f0aaf80c7b3f794f512c5f31
Abhinav Gupta <mail@abhinavg.net>
2023-02-24 17:14:50 -0800

    transform(link): Handle local fragment references
    For local fragment references (e.g. `#foo`),
    if they collide with other headers in the final document,
    use the new header IDs.

--- transform.go
+++ transform.go
@@ -67,15 +67,15 @@ func (t *transformer) transformFile(f *markdownFileItem) {
 		h.AST.Level += f.Item.Depth + t.sectionLevel
 	}
 
-	t.transformLink(".", f.Item.AST)
+	t.transformLink(".", f, f.Item.AST)
 
 	dir := filepath.Dir(f.Path)
 	for _, l := range f.Links {
-		t.transformLink(dir, l)
+		t.transformLink(dir, f, l)
 	}
 
 	for _, i := range f.Images {
-		t.transformImage(dir, i)
+		t.transformImage(dir, f, i)
 	}
 
 	doc := f.File.AST
@@ -86,38 +86,39 @@ func (t *transformer) transformFile(f *markdownFileItem) {
 	}
 }
 
-func (t *transformer) transformLink(fromDir string, link *ast.Link) {
-	link.Destination = []byte(t.transformURL(fromDir, string(link.Destination)))
+func (t *transformer) transformLink(fromDir string, f *markdownFileItem, link *ast.Link) {
+	link.Destination = []byte(t.transformURL(fromDir, f, string(link.Destination)))
 }
 
-func (t *transformer) transformImage(fromDir string, image *ast.Image) {
-	image.Destination = []byte(t.transformURL(fromDir, string(image.Destination)))
+func (t *transformer) transformImage(fromDir string, f *markdownFileItem, image *ast.Image) {
+	image.Destination = []byte(t.transformURL(fromDir, f, string(image.Destination)))
 }
 
-func (t *transformer) transformURL(fromDir, toURL string) string {
+func (t *transformer) transformURL(fromDir string, f *markdownFileItem, toURL string) string {
 	u, err := url.Parse(toURL)
 	if err != nil || u.Scheme != "" || u.Host != "" {
 		return toURL
 	}
 
-	if u.Path == "" {
-		// TODO: handle link to local header
-		return toURL
+	// Resolve the Path component of the URL to the destination file.
+	to := f
+	if u.Path != "" {
+		dst := filepath.Join(fromDir, u.Path)
+		var ok bool
+		to, ok = t.filesByPath[dst]
+		if !ok {
+			// This is a relative path that does not point to a Markdown
+			// file in the collection.
+			// It may be a link to a file in the input directory.
+			// Update the path and leave everything else as-is.
+			u.Path = path.Join(filepath.ToSlash(t.InputRelPath), dst)
+			return u.String()
+		}
+		u.Path = ""
 	}
 
-	dst := filepath.Join(fromDir, u.Path)
-	to, ok := t.filesByPath[dst]
-	if !ok {
-		// This is a relative path that does not point to a Markdown
-		// file in the collection.
-		// It may be a link to a file in the input directory.
-		u.Path = path.Join(filepath.ToSlash(t.InputRelPath), dst)
-		return u.String()
-	}
-
-	u.Path = ""
 	if u.Fragment != "" {
-		// If the fragment of a link to another Markdown file
+		// If the fragment of a link to a Markdown file
 		// is a known heading in that Markdown file,
 		// use the new ID of that header.
 		if h, ok := to.HeadingsByOldID[u.Fragment]; ok {
